eb3c4e4c21c446ba8e7292c2bc39a4e550e1856c
Miriam Frank <miriamfrank23@gmail.com>
2018-05-21 01:58:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const shout = (str) => {
-  return string.toUpperCase()
+  return str.toUpperCase()
 }
 
 const whisper = (str) => {
   return str.toLowerCase();
+}
+
+const logWhisper = (str) => {
+  console.log(str.toLowerCase());
+}
+
+const sayHiToGrandma = (str) => {
+  if (str === lowercase) {
+    return "I can't hear you!";
+  }
+  if (str === uppercase) {
+    return "YES INDEED";
+  }
+  if (str === 'I love you, Grandma.') {
+    return 'I love you, too.'
+  }
 }
